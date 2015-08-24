7e2a83d599669296f4c36c07a0817925097bd0dd
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-08-24 09:50:26 +0100

    T1 creating routes

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var port = process.env.PORT || 9000; 
+var port = process.env.PORT || 3000; 
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var expressLayouts = require('express-ejs-layouts');
+var router = express.Router();
+var userRouter = express.Router();
+var locationRouter = express.Router();
+mongoose.connect('mongodb://localhost/buckets');
 
-mongoose.connect('mongodb://localhost/buckets')
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-post('/',function(req,res))
-
-get('/'), function()
-{
-  
-}
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+app.set('layout', 'layout');
+app.set("views", "./views");
+app.set("view engine", "ejs");
