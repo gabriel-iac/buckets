b39d448005a104196f3df21fe2eb96c1ee779541
keithdhd <keith@keithdouglas.co.uk>
2015-08-24 09:56:05 +0100

    create location model

---
+++ models/location.js
@@ -0,0 +1,16 @@
+var mongoose  = require('mongoose');
+var User      = require('./user');
+var sport     = require('./sport');
+var country   = require('./country');
+
+
+var locationSchema = new mongoose.Schema({
+  name  : String,
+  country: { type: Schema.Types.ObjectId, ref: 'Country' }
+  sport: { type: Schema.Types.ObjectId, ref: 'Sport' }
+  long: String,
+  lat: String,
+  image: String,
+  users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  creator: { type: Schema.Types.ObjectId, ref: 'User' }
+}); 
