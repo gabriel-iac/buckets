735360cca36cea82bd7ff1642235e4d32f97c2d5
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-08-24 09:45:28 +0100

    T1 requiring files and creating db

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var port = process.env.PORT || 9000; 
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var expressLayouts = require('express-ejs-layouts');
 
+mongoose.connect('mongodb://localhost/buckets')
 
 post('/',function(req,res))
 
