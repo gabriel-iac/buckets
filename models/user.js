0a87fb18d5e2966c5a99e92b4504715f51135198
keithdhd <keith@keithdouglas.co.uk>
2015-08-24 09:48:24 +0100

    create user model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose   = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  first_name  : String,
+  last_name   : String,
+  image       : String,
+  email       : String,
+  password    : String,
+  locations   : [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+  fb: {
+    id: String,
+    access_token: String,
+    firstName: String,
+    lastName: String,
+    email: String
+  }
+})
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User; 
