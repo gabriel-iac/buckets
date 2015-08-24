708e48db5fe418082262b1cc84fea407df98fa40
marcusps11 <marcus@1roof.com>
2015-08-24 15:14:01 +0100

    form done

--- controllers/locations.js
+++ controllers/locations.js
@@ -1,19 +1,17 @@
 var Location = require('../models/location');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
 function getAllLocations(req, res){
   var locations = Location.find({} , function(err, locations){
     res.json(locations);
+  }); 
+};
 
-  }) 
-
-}
-
-
-// function getNewLocation(req, res){
-//   var 
-
-// }
+function getNewLocation(req,res){
+  console.log('hello')
+  res.render('locations/new');
+};
 
 function createLocation(req, res){
   //create new location object
@@ -26,20 +24,19 @@ function createLocation(req, res){
       if(err){console.log(err)}
         else{
           res.json(newlocation);
-        }
-    })
-}
+        };
+    });
+};
 
 function getLocation(req, res){
    Location.findById(req.params.id, function(err, location){
     res.json(location)
-
-  })
-
-}
+  });
+};
 
 module.exports = {
   getAllLocations: getAllLocations,
   createLocation: createLocation,
-  getLocation: getLocation
+  getLocation: getLocation,
+  getNewLocation: getNewLocation
 }
