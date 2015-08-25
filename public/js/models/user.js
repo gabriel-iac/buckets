790c36bca37ad43762f1b824786c5e1d92fbeb90
keithdhd <keith@keithdouglas.co.uk>
2015-08-25 13:21:11 +0100

    T8 ajax get users function

--- public/js/models/user.js
+++ public/js/models/user.js
@@ -2,13 +2,25 @@ function User(){
 
   this.getUsers = function(){
     console.log("Getting users");
-
+    
     //ajax call to get users
+     $.ajax({
+      url       : '/api/users',
+      dataType  : 'json',
+      beforeSend: function(xhr){
+        if($window.sessionStorage.token){
+          xhr.setRequestHeader("Authorization", "Bearer " + $window.sessionStorage.token);
+        }
+      },
+      error: function(){
+        //handle error
+      },
+      success: function(data){
+        //return a list of users
+        return data;
+      }
 
-    //return a list of users
-
-    //
-
+     });
   }
 
   this.createUser = function(){
@@ -16,4 +28,3 @@ function User(){
   }
 
 }
-
