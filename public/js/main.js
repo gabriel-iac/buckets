61af7a0781ad6d2844793b7299a2418c36d63a35
keithdhd <keith@keithdouglas.co.uk>
2015-08-25 14:28:44 +0100

    T8 init mainController with userToken

--- public/js/main.js
+++ public/js/main.js
@@ -3,19 +3,18 @@ function MainController(){
 }
 
 MainController.prototype.init = function(){ 
+  //if user is logged in get token
+  this.userToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NWRjMzcyNWI0M2IyMmI4NGI4MDY5ZmQiLCJmaXJzdF9uYW1lIjoiR2FicmllbGUiLCJsYXN0X25hbWUiOiJJYWNvcGV0dGkiLCJpbWFnZSI6ImltYWdlIiwiZW1haWwiOiJnYWJAZ2FiLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQiLCJfX3YiOjAsImxvY2F0aW9ucyI6W119.T61ZwiumFk6LYeknJ8f7FqqZlPRQQgrOjIugiS70BlA";
 }
 
 MainController.prototype.getUsers = function(){
-  return this.user.getUsers();
+  return this.user.getUsers(this.userToken);
 }
 
 
-
-
 $(function(){
-
   var mainController = mainController || new MainController();
-
-  $("#users").append("<li>" + mainController.getUsers() + "</li>");
-
+  
+  mainController.init();
+  mainController.getUsers();
 });
--- public/js/models/user.js
+++ public/js/models/user.js
@@ -1,15 +1,15 @@
 function User(){
 
-  this.getUsers = function(){
+  this.getUsers = function(userToken){
     console.log("Getting users");
-
+    console.log(userToken);
     //ajax call to get users 
      $.ajax({
       url       : '/api/users',
       dataType  : 'json',
       beforeSend: function(xhr){
         //if(''){
-          xhr.setRequestHeader("x-access-token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NWRjMzcyNWI0M2IyMmI4NGI4MDY5ZmQiLCJmaXJzdF9uYW1lIjoiR2FicmllbGUiLCJsYXN0X25hbWUiOiJJYWNvcGV0dGkiLCJpbWFnZSI6ImltYWdlIiwiZW1haWwiOiJnYWJAZ2FiLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQiLCJfX3YiOjAsImxvY2F0aW9ucyI6W119.T61ZwiumFk6LYeknJ8f7FqqZlPRQQgrOjIugiS70BlA");
+          xhr.setRequestHeader("x-access-token", userToken);
         //}
       },
       error: function(){
@@ -18,10 +18,11 @@ function User(){
       success: function(data){
         //return a list of users
         console.log(data);
-        return data;
+        for(var i=0; i<data.length; i++){
+          $("#users").append("<li>" + data[i].first_name + "</li>");
+        } 
       }
      });
-     
   }
 
   this.createUser = function(){
