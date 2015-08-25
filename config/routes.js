8217b861551971c70311d909fb72428b58bcbd91
keithdhd <keith@keithdouglas.co.uk>
2015-08-25 11:14:01 +0100

    T16 jwt protect user api

--- config/routes.js
+++ config/routes.js
@@ -10,25 +10,6 @@ var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 
-
-//USERS
-router.route('/users')
-.get(usersController.getAllUsers)
-.post(usersController.createUser)
-
-router.route('/users/:id')
-.put(usersController.updateUser)
-
-router.route('/users/:id')
-.get(usersController.showUser)
-
-// API routes
-
-// route to show a random message (GET http://localhost:8080/api/)
-router.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 router.post('/authenticate', function(req, res) {
 console.log(req.body.email);
@@ -66,6 +47,56 @@ console.log(req.body.email);
 
   });
 });
+
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, "iloveextremesport", function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+//USERS
+router.route('/users')
+.get(usersController.getAllUsers)
+.post(usersController.createUser)
+
+router.route('/users/:id')
+.put(usersController.updateUser)
+
+router.route('/users/:id')
+.get(usersController.showUser)
+
+// API routes
+
+// route to show a random message (GET http://localhost:8080/api/)
+router.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
 
 //LOCATIONS
 
