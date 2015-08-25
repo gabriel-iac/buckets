bd1d63f6e2b0efbd12516aec021c78b4da39ecde
marcusps11 <marcus@1roof.com>
2015-08-25 11:38:44 +0100

    added info marker

--- public/js/maps.js
+++ public/js/maps.js
@@ -2,7 +2,6 @@ function initialize() {
   var mapCanvas = document.getElementById('map');
   var lat = 44.5403;
   var long = -78.5463;
-  var position = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
     center: new google.maps.LatLng(lat, long),
     zoom: 8,
@@ -11,11 +10,22 @@ function initialize() {
   var map = new google.maps.Map(mapCanvas, mapOptions);
 
   var marker = new google.maps.Marker({
-         position: new google.maps.LatLng(lat, long),
-         map: map,
-         title:"This is the place."
+   position: new google.maps.LatLng(lat, long),
+   map: map,
+   title:"This is the place."
 
-})
+ })
+
+  var contentString = 'This is really fun for going skating';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+
 }
 
 
