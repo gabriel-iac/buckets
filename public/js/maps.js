b7f06511fe6a8de879116ad40a0cc21091fdf183
marcusps11 <marcus@1roof.com>
2015-08-25 14:36:09 +0100

    location search bar working

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,35 +1,82 @@
+var geocoder;
+var map;
+var places;
+var markers = [];
+
+
 function initialize() {
-  var mapCanvas = document.getElementById('map');
-  var lat = 44.5403;
-  var long = -78.5463;
-  var mapOptions = {
-    center: new google.maps.LatLng(lat, long),
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+// Create the Geocoder
+geocoder = new google.maps.Geocoder();
 
-  var marker = new google.maps.Marker({
-   position: new google.maps.LatLng(lat, long),
-   map: map,
-   title:"This is the place."
+var lat = 44.5403;
+var long = -78.5463;
 
- })
 
-  var contentString = 'This is really fun for going skating';
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+// Set map options
+var mapOptions = {
+  center: new google.maps.LatLng(lat, long),
+  zoom: 8,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+}
 
+
+// Create the map ref the map canvas
+var mapCanvas = document.getElementById('map');
+
+
+var map = new google.maps.Map(mapCanvas, mapOptions);
+
+
+// Fetch the places using ajax and put them on the map
+// fetchplaces() - FUNCTION THAT WE CALL LATER
+
+
+ //  var marker = new google.maps.Marker({
+ //   position: new google.maps.LatLng(lat, long),
+ //   map: map,
+ //   title:"This is the place."
+
+ // })
+
+
+
+
+ //  google.maps.event.addListener(marker, 'click', function() {
+ //    infowindow.open(map,marker);
+ //  });
+
+
+$("#submitLocation").on('click',function(){
+  event.preventDefault()
+  var loc = $("#location").val();
+  console.log(loc);
+
+  geocoder.geocode( { 'address': loc }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        // log out results from geocoding
+        console.log(results);
+        map.setCenter(results[0].geometry.location); }})
+
+
+})
 
 }
 
 
 
+
+
+
+var fetchPlaces = function() {
+
+  var contentString = 'This is really fun for going skating';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: ''
+  });
+
+}
 
 
 
